967c85c90799b5bdba2160c9ef339f0908aec2fc
Maria Angelova <angelova.m@hotmail.com>
2023-11-12 21:47:10 +0100

    feat: add dynamic routing

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -10,10 +10,12 @@ defineProps({
 </script>
 
 <template>
-  <div class="event-card">
-    <h2>{{ event.title }}</h2>
-    <span>@{{ event.time }} on {{ event.date }}</span>
-  </div>
+  <RouterLink :to="{ name: 'event-details', params: { id: event.id } }">
+    <div class="event-card">
+      <h2>{{ event.title }}</h2>
+      <span>@{{ event.time }} on {{ event.date }}</span>
+    </div>
+  </RouterLink>
 </template>
 
 <style scoped>
@@ -28,5 +30,10 @@ defineProps({
 .event-card:hover {
   transform: scale(1.01);
   box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+
+.event-link {
+  text-decoration: none;
+  color: #2c3e50;
 }
 </style>
